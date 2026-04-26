cc284ee412f148ce9a0b0ae690c7abf1879f807a
Anna <abhag18@gmail.com>
2026-04-26 17:00:38 -0400

    add playwright testsyml to project

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title1', async ({ page }) => {
   await page.goto('/',  { waitUntil: 'networkidle' });
   await expect(page).toHaveTitle(/Fodor's Travel/);
 });
